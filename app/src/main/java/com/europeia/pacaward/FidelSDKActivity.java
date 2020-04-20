dc745864f540b5dd1221b8e37b146b087364c38a
!null <franciscordeiro99@gmail.com>
2020-04-20 15:43:45 +0100

    minor changes

--- app/src/main/java/com/europeia/pacaward/FidelSDKActivity.java
+++ app/src/main/java/com/europeia/pacaward/FidelSDKActivity.java
@@ -12,7 +12,6 @@ import com.fidel.sdk.Fidel;
 public class FidelSDKActivity extends AppCompatActivity {
 
     private static final String TAG = "SDK";
-    private WebView sdkWebView;
 
 
     @Override
@@ -20,19 +19,12 @@ public class FidelSDKActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fidelsdk);
 
-        sdkWebView = findViewById(R.id.sdk);
-
-        //add web view
-
         Log.i(TAG, "onCreate: SDK");
         Fidel.programId = "dcf206fa-eeda-4e1d-b14d-0fd2f3bc7964";
         Fidel.apiKey = "pk_test_658d96a9-ea47-4aa2-bedc-57";
         Fidel.companyName = "pacaward";
 
-        WebSettings webSettings = sdkWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
 
-
-        //sdkWebView.loadData(Fidel.present(FidelSDKActivity.this),"text/javascript","UTF-8");
+        Fidel.present(FidelSDKActivity.this);
     }
 }
